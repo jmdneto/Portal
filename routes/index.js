08a4a77b7aae9daee1f7ce6ae0e759cd6f7d7df1
João Neto <jmdneto@gmail.com>
2018-03-21 17:25:52 -0300

    Teste pequena alteração em dev

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/02/generic.html', function(req, res, next) {
 });
 
 router.get('/02/elements.html', function(req, res, next) {
-  res.render('elements.html', { title: 'Express' });
+  res.render('elements.html', { title: 'Expres' });
 });
 
 router.get('/03', function(req, res, next) {
